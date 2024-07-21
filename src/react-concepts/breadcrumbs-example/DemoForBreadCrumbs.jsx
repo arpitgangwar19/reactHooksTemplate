13c5149da20468fa63a1c3889ed74f9f728cc44a
Aastha Gangwar <aastha.gangwar@oracle.com>
2024-07-21 11:38:46 +0530

    new hooks added

---
+++ src/react-concepts/breadcrumbs-example/DemoForBreadCrumbs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Index from './Index'
+import * as ReactDOM from "react-dom";
+import Breadcrumbs from './BreadCrumbs';
+
+const DemoForBreadCrumbs = () => {
+  return (
+   <>
+    <Breadcrumbs/>
+    <Index/></>
+  )
+}
+
+export default DemoForBreadCrumbs
